e4577e73340318a44e6172b846000991c5a9f037
AhmedAsar <ahmedasartech@outlook.com>
2025-10-25 23:53:45 +0300

    Add Customer,Product, Order

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import EmailValidator, MinValueValidator, RegexValidator
+from django.utils import timezone
+from decimal import Decimal
+
 
 # Create your models here.
+phone_validator = RegexValidator(
+    regex=r"^(\+\d{7,15}|\d{3}-\d{3}-\d{4})$",
+    message="Phone must be like +1234567890 or 123-456-7890",
+)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, validators=[EmailValidator()])
+    phone = models.CharField(
+        max_length=30, blank=True, null=True, validators=[phone_validator]
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal(0.01))]
+    )
+    stock = models.PositiveIntegerField(default=0)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="orders"
+    )
+    Product = models.ManyToManyField(Product, related_name="orders")()
+    order_date = models.DateTimeField(default=timezone.now)
+    total_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=Decimal(0.00)
+    )
