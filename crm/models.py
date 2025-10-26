11ca3a727f74ed3ab5a1dc1bdec57ccd90df3720
AhmedAsar <ahmedasartech@outlook.com>
2025-10-26 23:40:05 +0300

    Add __str__ methods to CRM models (Customer, Product, Order) to return name fields

--- crm/models.py
+++ crm/models.py
@@ -18,6 +18,9 @@ class Customer(models.Model):
         max_length=30, blank=True, null=True, validators=[phone_validator]
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -25,6 +28,9 @@ class Product(models.Model):
         max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal(0.01))]
     )
     stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -36,3 +42,6 @@ class Order(models.Model):
     total_amount = models.DecimalField(
         max_digits=10, decimal_places=2, default=Decimal(0.00)
     )
+
+    def __str__(self):
+        return f"Order {self.id} - {self.customer.name} - {self.total_amount}"
