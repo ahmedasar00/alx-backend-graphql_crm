0864e19fbf0ce219cdee39237f89f1da97a6f395
AhmedAsar <ahmedasartech@outlook.com>
2025-10-27 15:26:13 +0300

    Add Mutations to the Schema and Integrate into schema

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,10 +1,21 @@
 import graphene
 
-from crm.schema import Query as CRMQuery
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
 
-class Query(CRMQuery, graphene.ObjectType):
+class Query(graphene.ObjectType):
     hello = graphene.String(description="Hello, GraphQL! Ahmed 3sar")
 
 
 schema = graphene.Schema(query=Query)
+
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+
+class Mutation(graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- crm/schema.py
+++ crm/schema.py
@@ -1,13 +1,23 @@
 import graphene
 from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+from graphene import Mutation
+from .mutations import (
+    CreateCustomer,
+    BulkCreateCustomers,
+    CreateProduct,
+    CreateOrder,
+)
 
 
 class Query(CRMQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
-
-
 schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
+class Mutation(graphene.ObjectType):
+    create_customer = CreateCustomer.Field()
+    bulk_create_customers = BulkCreateCustomers.Field()
+    create_product = CreateProduct.Field()
+    create_order = CreateOrder.Field()
