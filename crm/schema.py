219f2141580f2046a6d85f287fa2d6b6f7aa1c5f
AhmedAsar <ahmedasartech@outlook.com>
2025-10-27 17:29:34 +0300

    feat(schema): add all_customers query and resolver in crm/schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
+
 # Types
 
 from crm.models import Customer, Product, Order
@@ -46,3 +47,11 @@ class OrderInput(graphene.InputObjectType):
     Customer_id = graphene.ID(required=True)
     Product_id = graphene.List(graphene.ID, required=True)
     order_date = graphene.types.datetime.DateTime(required=False)
+
+
+#!  QUERIES
+class Query(graphene.ObjectType):
+    all_customers = graphene.List(CustomerType)
+
+    def resolve_all_customers(root, info):
+        return Customer.objects.all()
